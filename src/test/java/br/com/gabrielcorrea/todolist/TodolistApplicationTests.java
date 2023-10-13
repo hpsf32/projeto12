76082b3505780fda5420e803d5f1253f290c18ed
hpsf32 <57626981+hpsf32@users.noreply.github.com>
2023-10-13 17:25:33 -0300

    Dockerfile deploy

---
+++ src/test/java/br/com/gabrielcorrea/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielcorrea.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
